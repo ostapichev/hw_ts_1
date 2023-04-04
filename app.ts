8462316515264d9bb22c1be592b47f4bc8d7f9f7
Ostapichev <ytox@ukr.net>
2023-04-04 23:38:24 +0300

    Starting project

---
+++ app.ts
@@ -0,0 +1,106 @@
+// 1) створити інтерфейс на основі цього объекта:
+// Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
+
+interface IProperty {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        "site_name_long": string;
+    };
+    links: {
+        article_link: string,
+        video_link: string
+    };
+    rocket: IRocket;
+}
+
+interface IRocket {
+    rocket_name: string;
+    first_stage: IFirst_stage;
+    second_stage: ISecond_stage;
+}
+
+interface IFirst_stage {
+    cores: [
+        {
+            flight: number,
+            core: {
+                reuse_count: number,
+                status: string
+            }
+        }
+    ]
+}
+
+interface ISecond_stage {
+    payloads: [
+        {
+            payload_type: string;
+            payload_mass_kg: number;
+            payload_mass_lbs: number;
+        }
+    ]
+}
+
+const property: IProperty = {
+    mission_name: "Starlink-15 (v1.0)",
+    launch_date_local: "2020-10-24T11:31:00-04:00",
+    launch_site: {
+        site_name_long: 'Cape Canaveral Air Force Station Space Launch Complex 40'
+    },
+    links: {
+        article_link: "http://some.com",
+        video_link: 'https://youtu/J442-ti-Dhg'
+    },
+    rocket: {
+        rocket_name: "Falcon 9",
+        first_stage: {
+            cores: [
+                        {
+                            flight: 7,
+                            core: {
+                                reuse_count: 6,
+                                status: 'unknown'
+                            }
+                        }
+                        ]
+        },
+        second_stage: {
+            payloads: [
+                    {
+                        payload_type: 'Satellite',
+                        payload_mass_kg: 15400,
+                        payload_mass_lbs: 33951.2
+                    }
+                    ]
+        }
+    }
+}
+
+console.log(property);
+
+//---------------------------------------------------------------------------—————————
+// 2) протипізувати функції:
+
+const user: {name: string, age: number, gender: string} = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
+
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+
+const showSum = (a: number, b: number): void => {
+    console.log(a + b);
+}
+
+const incAge = (someUser: any, inc: number): object => {
+    someUser.age += inc;
+    return someUser;
+}
+
+console.log(sum(1, 2));
+showSum(2,3)
+console.log(incAge(user, 2));
